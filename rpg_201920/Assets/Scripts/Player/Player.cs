2658c2c2a2b452f9f76d9571611f26fa5b55d62b
Marti <44981000+marti-toonen@users.noreply.github.com>
2019-12-09 08:21:00 -0500

    12062019

---
+++ rpg_201920/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float move_speed = 5;
+
+    void Update()
+    {
+        if(Input.GetAxis("Horizontal") > 0.5f || Input.GetAxis("Horizontal") <= 0.5f)
+            transform.Translate(new Vector2(Input.GetAxis("Horizontal"), 0f) * move_speed * Time.deltaTime);
+        if(Input.GetAxis("Vertical") > 0.5f || Input.GetAxis("Vertical") <= 0.5f)
+            transform.Translate(new Vector2(0f, Input.GetAxis("Vertical")) * move_speed * Time.deltaTime);
+    }
+}
